d5a9e68967100a2a496e2546f386f2991985f583
Marcin Zybura <marcin.zybura@skycash.com>
2018-12-11 11:25:24 +0100

    mobile menu

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function changeHamburger(x) {
   x.classList.toggle("change");
-  // $(".navigation-list-mobile").slideToggle();
+  $(".navigation-mobile").slideToggle();
+  $(".navigation").toggleClass("navigation--no-background");
+  $(".header").toggleClass("navigation--background-shadow");
 }
