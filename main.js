cd6d643b476afe9abc57cd272acdcc825b408d83
Marcin Zybura <marcin.zybura@skycash.com>
2018-12-11 13:08:57 +0100

    popup

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ function changeHamburger(x) {
   $(".navigation").toggleClass("navigation--no-background");
   $(".header").toggleClass("navigation--background-shadow");
 }
+
+$("#title-section__button").on("click", () => {
+  $(".shadow").toggle();
+  $(".popup").fadeIn();
+});
+
+$(document).mouseup(function(e) 
+{
+    let container = $(".popup");
+    console.log(e.target);
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        container.fadeOut();
+        $(".shadow").hide();
+        console.log(e.target);
+    }
+});
